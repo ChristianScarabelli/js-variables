2a62b7ac09059adfa468c233fe53866a533e77c0
ChristianScarabelli <scachmabol@hotmail.it>
2024-10-14 10:08:41 +0200

    ADD: bonus corrections

--- 03-strings-bonus/script.js
+++ 03-strings-bonus/script.js
@@ -6,17 +6,17 @@ const hello = 'hello'; // NON MODIFICARE QUESTA RIGA
 // 1. Cerca un carattere in una stringa
 // Stampa l'INDICE con cui viene trovato
 // il carattere Y nella variabile city
-const cityIndex = city.at(4);
+const cityIndex = city.indexOf('Y');
 console.log(cityIndex); // Output: 4
 
 // 2. Sottostringhe 
 // Stampa la sottostringa "York" dalla variabile city
-const citySubstring = city.slice(4);
+const citySubstring = city.slice(cityIndex);
 console.log(citySubstring); // Output: "York"
 
 // 3. Sostituzione
 // Sostituisci 'York' con 'Delhi'
-const cityReplaced = city.replace('York', 'Delhi');
+const cityReplaced = city.replace(citySubstring, 'Delhi');
 console.log(cityReplaced); // Output:  "New Delhi"
 
 // 4. Template literal
